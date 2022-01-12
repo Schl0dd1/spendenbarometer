ed4b3102a75742b0d3b3c9b62a24282fff78c932
kalli <83395926+Schl0dd1@users.noreply.github.com>
2022-01-12 12:45:57 +0100

    einnahme-ausgabe-button added

--- frontend/src/components/KontoCard.svelte
+++ frontend/src/components/KontoCard.svelte
@@ -1,12 +1,35 @@
 <script>
 	export let kontoname;
 	export let kontostand;
+	export let src;
 	export let betrag;
 	import { fade } from 'svelte/transition';
 
-	function handleClick() {
-		kontostand += betrag;
+	let style = 'uppercase text-2xl';
+
+	let update = () => {
+		betrag = 0;
+		style = kontostand < 0 ? 'uppercase text-2xl text-rose-600' : 'uppercase text-2xl';
+	};
+
+	function handleClickAusgabe() {
+		if (betrag < 0) {
+			betrag = betrag * -1;
+		}
+		kontostand -= betrag;
+		update();
 	}
+
+	function handleClickEinnahme() {
+		if (betrag < 0) {
+			betrag = betrag * -1;
+		}
+		kontostand += betrag;
+		update();
+	}
+
+	const age = 20;
+	const age_group = age < 18 ? 'Child' : 'Adult';
 </script>
 
 <div
@@ -14,7 +37,8 @@
 	href={``}
 	transition:fade
 >
-	<img class="h-40" src="../img/the-bird.webp" alt="Foto" />
+	<h1 class="uppercase text-2xl">{kontoname}</h1>
+	<img class="h-40" {src} alt="Foto" />
 	<div class="mb-3 xl:w-96">
 		<label for="exampleNumber0" class="form-label inline-block mb-2 text-gray-700"
 			>Gib einen Betrag in € ein:</label
@@ -29,7 +53,7 @@
             py-1.5
             text-base
             font-normal
-            text-gray-700
+            text-grey-700
             bg-white bg-clip-padding
             border border-solid border-gray-300
             rounded
@@ -43,12 +67,20 @@
 			bind:value={betrag}
 		/>
 	</div>
-	<button
-		on:click={handleClick}
-		class="my-4 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-	>
-		Eintrag hinzufügen
-	</button>
-	<h1 class="uppercase text-2xl">{kontoname}</h1>
-	<h2 class="uppercase text-1xl">{kontostand} €</h2>
+	<div>
+		<button
+			on:click={handleClickAusgabe}
+			class="my-4 bg-transparent hover:bg-rose-500 text-rose-700 font-semibold hover:text-white py-2 px-4 border border-rose-600 hover:border-transparent rounded"
+		>
+			Ausgabe
+		</button>
+		<button
+			on:click={handleClickEinnahme}
+			class="my-4 bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-600 hover:border-transparent rounded"
+		>
+			Einnahme
+		</button>
+	</div>
+
+	<h2 class={style}>{kontostand} €</h2>
 </div>
--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,13 +1,16 @@
 <script>
 	import KontoCard from '../components/KontoCard.svelte';
 
+	//durch daten aus datenbank ersetzen:
 	let k1 = {
 		kontoname: 'Hanni',
-		kontostand: 0
+		kontostand: 0,
+		src_img: '../img/the-bear.webp'
 	};
 	let k2 = {
 		kontoname: 'Nanni',
-		kontostand: 30
+		kontostand: 30,
+		src_img: '../img/the-the-cat.webp'
 	};
 </script>
 
@@ -16,6 +19,6 @@
 
 <div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
 	<!-- wird später durch each as ersetzt -->
-	<KontoCard kontoname={k1.kontoname} kontostand={k1.kontostand} />
-	<KontoCard kontoname={k2.kontoname} kontostand={k2.kontostand} />
+	<KontoCard kontoname={k1.kontoname} kontostand={k1.kontostand} src={k1.src_img} />
+	<KontoCard kontoname={k2.kontoname} kontostand={k2.kontostand} src={k2.src_img} />
 </div>
