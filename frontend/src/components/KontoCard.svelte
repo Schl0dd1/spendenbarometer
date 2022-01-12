51ec93928853c38722ec5583391f6a82b80eb846
kalli <83395926+Schl0dd1@users.noreply.github.com>
2022-01-12 11:18:15 +0100

    updating svelte index page

--- frontend/src/components/KontoCard.svelte
+++ frontend/src/components/KontoCard.svelte
@@ -1,15 +1,54 @@
 <script>
 	export let kontoname;
 	export let kontostand;
+	export let betrag;
 	import { fade } from 'svelte/transition';
+
+	function handleClick() {
+		kontostand += betrag;
+	}
 </script>
 
-<a
+<div
 	class="list-none p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center"
 	href={``}
 	transition:fade
 >
 	<img class="h-40" src="../img/the-bird.webp" alt="Foto" />
+	<div class="mb-3 xl:w-96">
+		<label for="exampleNumber0" class="form-label inline-block mb-2 text-gray-700"
+			>Gib einen Betrag in € ein:</label
+		>
+		<input
+			type="number"
+			class="
+            form-control
+            block
+            w-full
+            px-3
+            py-1.5
+            text-base
+            font-normal
+            text-gray-700
+            bg-white bg-clip-padding
+            border border-solid border-gray-300
+            rounded
+            transition
+            ease-in-out
+            m-0
+            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+          "
+			id="betrag"
+			placeholder="0.00"
+			bind:value={betrag}
+		/>
+	</div>
+	<button
+		on:click={handleClick}
+		class="my-4 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+	>
+		Eintrag hinzufügen
+	</button>
 	<h1 class="uppercase text-2xl">{kontoname}</h1>
-	<h2 class="uppercase text-1xl">{kontostand}</h2>
-</a>
+	<h2 class="uppercase text-1xl">{kontostand} €</h2>
+</div>
--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,5 +1,14 @@
 <script>
 	import KontoCard from '../components/KontoCard.svelte';
+
+	let k1 = {
+		kontoname: 'Hanni',
+		kontostand: 0
+	};
+	let k2 = {
+		kontoname: 'Nanni',
+		kontostand: 30
+	};
 </script>
 
 <h1 class="text-4xl text-center my-8 uppercase">Taschengeldapp</h1>
@@ -7,6 +16,6 @@
 
 <div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
 	<!-- wird später durch each as ersetzt -->
-	<KontoCard kontoname="Hanni" kontostand="0,00€" />
-	<KontoCard kontoname="Nanni" kontostand="20,00€" />
+	<KontoCard kontoname={k1.kontoname} kontostand={k1.kontostand} />
+	<KontoCard kontoname={k2.kontoname} kontostand={k2.kontostand} />
 </div>
